f39d8975d90239ee3fe4a246f0d8b51f14786a32
NessunKim <nessunkim@gmail.com>
2017-09-18 09:18:21 -0700

    toolbox dropdown

--- components/ToolBox.vue
+++ components/ToolBox.vue
@@ -1,35 +1,42 @@
 <template lang="pug">
-.tabs.is-toggle
-  ul(v-if="allowedActions.includes('article:read')")
-    nuxt-link(
-      tag="li"
-      :to="`/article/${encodedFullTitle}`"
-    )
-      a 읽기
-    nuxt-link(
-      v-if="allowedActions.includes('article:edit')"
-      tag="li"
-      :to="`/edit/${encodedFullTitle}`"
-    )
-      a 편집
-    nuxt-link(
-      v-else
-      tag="li"
-      :to="`/edit/${encodedFullTitle}`"
-    )
-      a 원본 보기
-    nuxt-link(
-      v-if="allowedActions.includes('discussion:read')"
-      tag="li"
-      :to="`/topics/${encodedFullTitle}`"
-    )
-      a 토의
-    nuxt-link(
-      v-if="allowedActions.includes('article:read')"
-      tag="li"
-      :to="`/history/${encodedFullTitle}`"
-    )
-      a 역사
+.tool-box
+  .tabs.is-toggle
+    ul(v-if="allowedActions.includes('article:read')")
+      nuxt-link(
+        tag="li"
+        :to="`/article/${encodedFullTitle}`"
+      )
+        a 읽기
+      nuxt-link(
+        v-if="allowedActions.includes('article:edit')"
+        tag="li"
+        :to="`/edit/${encodedFullTitle}`"
+      )
+        a 편집
+      nuxt-link(
+        v-else
+        tag="li"
+        :to="`/edit/${encodedFullTitle}`"
+      )
+        a 원본 보기
+      nuxt-link(
+        v-if="allowedActions.includes('discussion:read')"
+        tag="li"
+        :to="`/topics/${encodedFullTitle}`"
+      )
+        a 토의
+      nuxt-link(
+        v-if="allowedActions.includes('article:read')"
+        tag="li"
+        :to="`/history/${encodedFullTitle}`"
+      )
+        a 역사
+  b-dropdown(position="is-bottom-left")
+    .button.dropdown-button(slot="trigger")
+      b-icon(icon="caret-down")
+    b-dropdown-item 역링크
+    b-dropdown-item 문서명 변경
+    b-dropdown-item 삭제
 </template>
 
 <script>
@@ -55,5 +62,28 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+@import '~assets/style-variables.scss';
+
+.tool-box {
+  display: flex;
+  .tabs {
+    margin-bottom: 0;
+  }
+  .tabs.is-toggle li:last-child a {
+    border-radius: 0;
+  }
+  .dropdown {
+    margin-left: -1px;
+    display: flex;
+    .dropdown-trigger {
+      display: flex;
+      align-items: stretch;
+      .dropdown-button {
+        border-radius: 0 $radius $radius 0;
+        height: auto;
+      }
+    }
+  }
+}
 </style>
