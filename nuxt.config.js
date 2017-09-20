e079e0c3c1c75893b3712a496628e3321ac5eb10
NessunKim <nessunkim@gmail.com>
2017-09-20 09:38:02 -0700

    fix scrollbehavior

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,6 +48,27 @@ module.exports = {
     duration: '100'
   },
   router: {
-    linkActiveClass: 'is-active'
+    linkActiveClass: 'is-active',
+    scrollBehavior (to, from, savedPosition) {
+      // savedPosition is only available for popstate navigations.
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        let position = {}
+        // if no children detected
+        if (to.matched.length < 2) {
+          // scroll to the top of the page
+          position = { x: 0, y: 0 }
+        } else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+          // if one of the children has scrollToTop option set to true
+          position = { x: 0, y: 0 }
+        }
+        // if link has anchor,  scroll to anchor by returning the selector
+        if (to.hash) {
+          position = { selector: to.hash.replace(/\./g, '\\.') }
+        }
+        return position
+      }
+    }
   }
 }
