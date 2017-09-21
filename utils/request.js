dc7f902f97b149493aa40e3b1df9ee440862ebb0
NessunKim <nessunkim@gmail.com>
2017-09-22 02:01:34 +0900

    fix request

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import jwtDecode from 'jwt-decode'
 import Cookies from 'js-cookie'
-const prefix = 'http://localhost:3001'
+const prefix = '/api'
 
 async function request ({
   path,
