4bae29ae304aeb24b9771f60005e712c4147d557
NessunKim <nessunkim@gmail.com>
2017-10-02 20:02:15 +0900

    heading style

--- components/WikiHtml.vue
+++ components/WikiHtml.vue
@@ -53,8 +53,34 @@ export default {
     line-height: 1.5;
   }
 
-  h2 {
+  h1, h2, h3, h4, h5, h6 {
     margin: 0.5rem 0;
+    font-weight: 500;
+    line-height: 1.1;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.8rem;
+  }
+
+  h3 {
+    font-size: 1.6rem;
+  }
+
+  h4 {
+    font-size: 1.5rem;
+  }
+
+  h5 {
+    font-size: 1.3rem;
+  }
+
+  h6 {
+    font-size: 1.1rem;
   }
 
   p {
