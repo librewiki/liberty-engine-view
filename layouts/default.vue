ae709f0e39786cd37ec0b9d1ada541200f8aa3c6
NessunKim <nessunkim@gmail.com>
2017-10-03 23:08:34 +0900

    fix errorpage style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,15 @@
 <template lang="pug">
-#app.app
-  navbar
-  .main-container.container
-    .columns.main-columns
-      .column.main-column
-        site-notice
-        article.box.is-paddingless
-          header.main-header
-            .level.title-wrapper-row
-              .level-left
-                .level-item
-                  h1.title {{ meta.title }}
-              .level-right(v-if="meta.toolBox")
-                .level-item
-                  tool-box
-          section.main-content
-            nuxt
-          liberty-footer.footer.main-footer
-      .column.is-narrow.is-hidden-touch
-        live-recent
+layout
+  nuxt
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import Navbar from '~/components/Navbar'
-import ToolBox from '~/components/ToolBox'
-import LibertyFooter from '~/components/LibertyFooter'
-import SiteNotice from '~/components/SiteNotice'
-import LiveRecent from '~/components/LiveRecent'
+import Layout from '~/components/Layout'
 
 export default {
   components: {
-    SiteNotice,
-    Navbar,
-    ToolBox,
-    LibertyFooter,
-    LiveRecent
+    Layout
   },
   computed: {
     ...mapState(['meta', 'settings'])
@@ -48,97 +22,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-@import '~assets/style-variables.scss';
-@import "~bulma";
-@import "~buefy/src/scss/buefy";
-
-* {
-  outline: none;
-}
-
-html {
-  background-color: #f5f5f5;
-  overflow: auto;
-  -webkit-overflow-scrolling: touch;
-}
-
-.right-wrapper {
-  display: flex;
-  justify-content: flex-end;
-}
-
-.app {
-  .main-container {
-    margin-top: ($navbar-height + 1rem);
-    margin-bottom: 1rem;
-    @include touch {
-      margin-top: ($navbar-height * 2 + 0.5rem);
-    }
-  }
-  .main-columns {
-    @include touch {
-      margin-left: 0;
-      margin-right: 0;
-    }
-  }
-  .main-column {
-    @include touch {
-      padding-left: 0;
-      padding-right: 0;
-    }
-  }
-  .liberty-navbar {
-    position: fixed;
-    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
-  }
-  .main-header {
-    padding: $box-padding;
-    @include touch {
-      padding: ($box-padding / 2);
-    }
-    border-top-left-radius: $radius-large;
-    border-top-right-radius: $radius-large;
-    background-color: $background;
-    .title-wrapper-row {
-      @include mobile {
-        display: flex;
-        flex-direction: column-reverse;
-        align-items: stretch;
-        .level-left {
-          display: flex;
-          align-items: flex-start;
-        }
-        .level-right {
-          margin-top: 0;
-          padding-bottom: 0.5rem;
-          margin-bottom: 0.5rem;
-          display: flex;
-          justify-content: flex-end;
-          border-bottom: 1px solid #e1e8ed;
-        }
-      }
-    }
-  }
-  .main-content {
-    padding: $box-padding;
-    @include touch {
-      padding: ($box-padding / 2);
-    }
-  }
-  .main-footer {
-    padding: $box-padding;
-    @include touch {
-      padding: ($box-padding / 2);
-    }
-    border-bottom-left-radius: $radius-large;
-    border-bottom-right-radius: $radius-large;
-    background-color: $background;
-  }
-}
-
-a.new {
-  color: $newlink-color;
-}
-</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,26 +1,29 @@
-<template>
-  <div class="error-page">
-    <div>
-      <h1 class="error-code">{{ error.statusCode }}</h1>
-      <div class="error-wrapper-message">
-        <h2 class="error-message">{{ error.message }}</h2>
-        <div v-if="error.createArticleLink"><nuxt-link :to="error.createArticleLink">문서를 작성해</nuxt-link> 보세요.</div>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+layout
+  .page.page-error
+    h2.error-messgae {{ error.message }}
+    p(v-if="error.createArticleLink")
+      nuxt-link(:to="error.createArticleLink") 문서를 작성해
+      = " "
+      | 보세요.
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Layout from '~/components/Layout'
+
 export default {
   props: ['error'],
-  layout: 'default',
+  components: {
+    Layout
+  },
+  computed: {
+    ...mapState(['meta', 'settings'])
+  },
   head () {
     return {
-      title: this.error.message || 'An error occured'
+      title: this.error.message || '에러가 발생했습니다.'
     }
   }
 }
 </script>
-
-<style>
-</style>
