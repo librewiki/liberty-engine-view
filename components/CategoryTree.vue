14183f309129d6df1765751e0232d60c2834e8cb
NessunKim <nessunkim@gmail.com>
2017-10-09 16:42:02 +0900

    add message when there're no member articles in a category

--- components/CategoryTree.vue
+++ components/CategoryTree.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
 .category-tree
-  h3.is-size-3 하위 분류
-  ul.subcategories
-    li.subcategory(v-for="subcategory in subcategories")
-      nuxt-link(:to="`/article/${encodeURIComponent('분류:' + subcategory)}`") {{ subcategory }}
-  hr
+  template(v-if="subcategories.length")
+    h3.is-size-3 하위 분류
+    ul.subcategories
+      li.subcategory(v-for="subcategory in subcategories")
+        nuxt-link(:to="`/article/${encodeURIComponent('분류:' + subcategory)}`") {{ subcategory }}
+    hr
   h3.is-size-3 이 분류에 속하는 문서
-  ul.members
+  ul.members(v-if="members.length")
     li.member(v-for="member in members")
       nuxt-link(:to="`/article/${encodeURIComponent(member)}`") {{ member }}
+  p(v-else)
+    | 이 분류에 속하는 문서가 없습니다.
 </template>
 
 <script>
