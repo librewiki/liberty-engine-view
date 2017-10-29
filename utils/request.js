fabde933c0a1a8e50f8a98f2f9445d8d86868765
NessunKim <nessunkim@gmail.com>
2017-10-29 12:54:34 +0900

    update request method

--- utils/request.js
+++ utils/request.js
@@ -2,7 +2,70 @@ import axios from 'axios'
 import jwtDecode from 'jwt-decode'
 import Cookies from 'js-cookie'
 
-const prefix = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3001'
+const serverSideApiPath = 'http://localhost:3001'
+const clientSideApiPath = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3001'
+
+async function serverSideRequest ({
+  path,
+  version = 1,
+  method = 'get',
+  body,
+  req,
+  res
+}) {
+  const headers = {}
+  const xff = req.headers['x-forwarded-for']
+  headers['X-Forwarded-For'] = xff ? `${xff}, ${req.connection.remoteAddress}` : req.connection.remoteAddress
+  if (req.headers.cookie) {
+    const jwtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('jwt='))
+    if (jwtCookie) {
+      const token = jwtCookie.split('=')[1]
+      if (token) {
+        const decoded = jwtDecode(token)
+        if (decoded.exp < Date.now() / 1000) {
+          if (res) {
+            res.setHeader('Set-Cookie', ['jwt='])
+          }
+        } else {
+          headers.authorization = `Bearer ${token}`
+        }
+      }
+    }
+  }
+  if (path[0] === '/') {
+    path = path.slice(1)
+  }
+  const url = `${serverSideApiPath}/v${version}/${path}`
+  return axios({
+    url,
+    method,
+    data: body,
+    headers
+  })
+}
+
+async function clientSideRequest ({
+  path,
+  version = 1,
+  method = 'get',
+  body
+}) {
+  const headers = {}
+  const token = Cookies.get('jwt')
+  if (token) {
+    headers.authorization = `Bearer ${token}`
+  }
+  if (path[0] === '/') {
+    path = path.slice(1)
+  }
+  const url = `${clientSideApiPath}/v${version}/${path}`
+  return axios({
+    url,
+    method,
+    data: body,
+    headers
+  })
+}
 
 async function request ({
   path,
@@ -12,42 +75,26 @@ async function request ({
   req,
   res
 }) {
-  const headers = {}
-  if (process.server && req) {
-    const xff = req.headers['x-forwarded-for']
-    headers['X-Forwarded-For'] = xff ? `${xff}, ${req.connection.remoteAddress}` : req.connection.remoteAddress
-    if (req.headers.cookie) {
-      const jwtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('jwt='))
-      if (jwtCookie) {
-        const token = jwtCookie.split('=')[1]
-        if (token) {
-          const decoded = jwtDecode(token)
-          if (decoded.exp < Date.now() / 1000) {
-            if (res) {
-              res.setHeader('Set-Cookie', ['jwt='])
-            }
-          } else {
-            headers.authorization = `Bearer ${token}`
-          }
-        }
-      }
+  if (process.server) {
+    if (!req || !res) {
+      throw new Error('Must pass req and res')
     }
+    return serverSideRequest({
+      path,
+      version,
+      method,
+      body,
+      req,
+      res
+    })
   } else {
-    const token = Cookies.get('jwt')
-    if (token) {
-      headers.authorization = `Bearer ${token}`
-    }
+    return clientSideRequest({
+      path,
+      version,
+      method,
+      body
+    })
   }
-  if (path[0] === '/') {
-    path = path.slice(1)
-  }
-  const url = `${prefix}/v${version}/${path}`
-  return axios({
-    url,
-    method,
-    data: body,
-    headers
-  })
 }
 
 export default request
