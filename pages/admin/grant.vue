2e89070a2f868edd497bf2b648f6e6fe4f36c809
NessunKim <nessunkim@gmail.com>
2017-11-09 19:55:27 +0900

    impl grant role

---
+++ pages/admin/grant.vue
@@ -0,0 +1,106 @@
+<template lang="pug">
+.admin-grant
+  p 사용자에게 특정한 역할을 부여하거나 해제할 수 있습니다.
+  section.section-search
+    b-field(label="사용자 검색" @keyup.native.enter="search" message="역할을 설정할 사용자명을 입력해 주세요.")
+      b-autocomplete(
+        v-model="usernameToSearch"
+        :data="usernameSuggestions"
+        icon="search"
+      )
+    button.button.is-primary(@click="search") 찾기
+  section.section-form(v-if="targetUser")
+    .field(v-for="role in model.roles")
+      b-checkbox(v-model="role.checked") {{ role.name }}
+    button.button.is-primary(@click="submit") 적용
+</template>
+
+<script>
+import _ from 'lodash'
+import request from '~/utils/request'
+
+export default {
+  async asyncData ({ params, req, res, error, store, redirect }) {
+    store.commit('meta/clear')
+    store.commit('meta/update', {
+      title: '관리자 페이지 - 사용자 역할 부여 / 해제'
+    })
+    const { data: { roles } } = await request({
+      method: 'get',
+      path: 'roles',
+      req,
+      res
+    })
+    return { roles }
+  },
+  data () {
+    return {
+      usernameToSearch: '',
+      usernameSuggestions: [],
+      targetUser: null,
+      model: {
+        roles: {}
+      }
+    }
+  },
+  methods: {
+    async search () {
+      const { data: { users: [targetUser] } } = await request({
+        method: 'get',
+        path: 'users',
+        query: {
+          username: this.usernameToSearch
+        }
+      })
+      if (!targetUser) {
+        this.$toast.open({
+          duration: 3000,
+          message: '해당 유저는 존재하지 않습니다.',
+          type: 'is-danger'
+        })
+        return
+      }
+      this.targetUser = targetUser
+      this.model.roles = this.roles.map((role) => {
+        const checked = !!targetUser.roles.find(userRole => userRole.id === role.id)
+        return { ...role, checked }
+      })
+    },
+    async submit () {
+      const roleIds = this.model.roles.filter(role => role.checked).map(role => role.id)
+      await request({
+        method: 'put',
+        path: `users/${this.targetUser.id}/roles`,
+        body: { roleIds }
+      })
+      this.$toast.open({
+        duration: 3000,
+        message: '성공했습니다.',
+        type: 'is-success'
+      })
+    }
+  },
+  watch: {
+    usernameToSearch: _.debounce(async function () {
+      if (!this.usernameToSearch) return
+      const resp = await request({
+        method: 'get',
+        path: `users`,
+        query: {
+          startingWith: this.usernameToSearch,
+          limit: 20
+        }
+      })
+      this.usernameSuggestions = resp.data.users.map(targetUser => targetUser.username)
+    }, 200)
+  }
+}
+</script>
+
+<style lang="scss">
+.admin-grant {
+  .section-form {
+    margin-top: 1rem;
+  }
+}
+</style>
