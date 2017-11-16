732882e4ab51f8336d656f9915bd2b3b049d3a45
NessunKim <nessunkim@gmail.com>
2017-11-16 21:16:22 +0900

    fix redirection page text

--- pages/redirections/_fullTitle.vue
+++ pages/redirections/_fullTitle.vue
@@ -5,10 +5,10 @@
       b-table-column(label="이름") {{ props.row.sourceFullTitle }}
       b-table-column(label="삭제")
         button.button.is-danger(@click="deleteRedirection(props.row.sourceFullTitle)") 삭제
-    template(slot="empty") 이 문서로 넘겨주는 문서가 없습니다.
+    template(slot="empty") 이 문서로 가는 넘겨주기가 존재하지 않습니다.
   hr
   h3.is-size-3 새 넘겨주기 만들기
-  b-field(:label="`\"${article.fullTitle}\"에 넘겨줄 문서명`")
+  b-field(label="넘겨줄 이름")
     b-input(v-model="model.redirectionName")
   .right-wrapper
     button.button.is-primary(@click="addRedirection") 저장
@@ -33,7 +33,7 @@ export default {
     store.commit('meta/clear')
     const fullTitle = params.fullTitle
     store.commit('meta/update', {
-      title: `"${fullTitle}" 로 넘겨주는 문서들`
+      title: `"${fullTitle}" 문서로 넘겨주기`
     })
     try {
       const article = await articleManager.getByFullTitle(fullTitle, {
@@ -47,7 +47,7 @@ export default {
         res
       })
       store.commit('meta/update', {
-        title: `"${article.fullTitle}" 로 넘겨주는 문서들`,
+        title: `"${article.fullTitle}" 문서로 넘겨주기`,
         toolBox: {
           allowedActions: article.allowedActions,
           fullTitle: article.fullTitle
