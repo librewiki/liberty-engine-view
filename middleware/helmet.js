8268d2ec8ff1920480d84fe14ef077189f4164d8
NessunKim <nessunkim@gmail.com>
2018-04-27 00:46:42 +0900

    add content-security-policy

--- middleware/helmet.js
+++ middleware/helmet.js
@@ -1,5 +1,9 @@
 const helmet = require('helmet')
 
+const defaultSrc = process.env.NODE_ENV === 'production'
+  ? ["'self'", 'data:', 'www.gravatar.com']
+  : ["'self'", 'data:', 'www.gravatar.com', 'localhost:3001']
+
 module.exports = helmet({
   frameguard: {
     action: 'deny'
@@ -8,5 +12,17 @@ module.exports = helmet({
     maxAge: 63072000,
     includeSubDomains: true,
     preload: true
+  },
+  contentSecurityPolicy: {
+    directives: {
+      frameAncestors: ["'none'"],
+      defaultSrc,
+      fontSrc: ["'self'", 'cdnjs.cloudflare.com'],
+      styleSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com'],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      mediaSrc: ['*'],
+      objectSrc: ["'none'"],
+      workerSrc: ["'self'"]
+    }
   }
 })
