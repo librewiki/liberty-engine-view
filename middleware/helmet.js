ee913473931ccf5cf8adbed8995c8fcd531c9c26
NessunKim <nessunkim@gmail.com>
2020-05-13 22:46:24 +0900

    Remove csp rules

--- middleware/helmet.js
+++ middleware/helmet.js
@@ -1,9 +1,5 @@
 import helmet from 'helmet'
 
-const defaultSrc = process.env.NODE_ENV === 'production'
-  ? ["'self'", 'data:', 'www.gravatar.com']
-  : ["'self'", 'data:', 'www.gravatar.com', `http://localhost:${process.env.API_PORT || '3001'}`]
-
 export default helmet({
   frameguard: {
     action: 'deny'
@@ -12,17 +8,5 @@ export default helmet({
     maxAge: 63072000,
     includeSubDomains: true,
     preload: true
-  },
-  contentSecurityPolicy: {
-    directives: {
-      frameAncestors: ["'none'"],
-      defaultSrc,
-      fontSrc: ["'self'", 'cdnjs.cloudflare.com', 'cdn.jsdelivr.net'],
-      styleSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com', 'cdn.jsdelivr.net'],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      mediaSrc: ['*'],
-      objectSrc: ["'none'"],
-      workerSrc: ["'self'"]
-    }
   }
 })
